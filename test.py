4b1c0ac551254db48075bf0dcf77b4c20ab268d4
Nikita Win10 <slimnik47@gmail.com>
2022-10-10 14:41:46 +0300

    Test.py added

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-a = 3
-b = 7
-c = a + b
-for c in range(5):
-    print("Current number is ", c)
-    c += 1
-print("Done")
-a, b, i, t = 0, 1, 0, 0
-print("Fibonacci sequence:")
-for i in range(19):
-    print(a)
-    t = a
-    a = b
-    b = t+b
-    i =+ 1
+Some new files
